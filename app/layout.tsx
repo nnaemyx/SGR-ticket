b4d494c21bca93b8a9ef612202dae2fb55adf475
johnpaul <edehjohnpaul@gmail.com>
2025-04-19 03:53:23 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,10 @@ import localFont from 'next/font/local'
 export const metadata: Metadata = {
   title: "Lagos in Port Harcourt",
   description: "Get your tickets for the lifestyle party in Port Harcourt",
+  icons: {
+    icon: '/images/SGR LOGO MAIN copy 1.png',
+    apple: '/images/SGR LOGO MAIN copy 1.png',
+  },
 };
 
 const myFont = localFont({
@@ -42,6 +46,8 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <script src="https://js.paystack.co/v1/inline.js" />
+        <link rel="icon" href="/images/SGR LOGO MAIN copy 1.png" />
+        <link rel="apple-touch-icon" href="/images/SGR LOGO MAIN copy 1.png" />
       </head>
       <body className={myFont.className}>
         {children}
